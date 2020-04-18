987084daa4a15d5b17f7559f8b14f3671e5a0074
Gordon Koo <gordon@opendoor.com>
2020-04-17 18:37:10 -0700

    introduce leaders

--- be/player.js
+++ be/player.js
@@ -1,9 +1,18 @@
 function Player(id) {
   this.id = id;
+  this.isLeader = false;
 
   this.setName = name => {
     this.name = name;
   };
+
+  this.promoteToLeader = () => {
+    this.isLeader = true;
+  };
+
+  this.unpromoteFromLeader = () => {
+    this.isLeader = false;
+  }
 }
 
 module.exports = Player;
--- be/room.js
+++ be/room.js
@@ -6,11 +6,32 @@ function Room() {
   this.addPlayer = (id) => {
     const player = new Player(id);
     this.players[id] = player;
+    if (this.getPlayers().length === 1) {
+      this.promoteRandomLeader();
+    }
   };
 
   this.removePlayer = id => {
+    const player = this.players[id];
     delete this.players[id];
+    if (player.isLeader) {
+      this.promoteRandomLeader();
+    }
   };
+
+  this.promoteRandomLeader = () => {
+    const players = this.getPlayers();
+
+    if (players.length === 0) {
+      return;
+    }
+
+    players[0].promoteToLeader();
+    console.log(`${players[0].name || players[0].id} is the new leader`);
+    for (let i = 1; i < players.length; ++i) {
+      players[i].unpromoteFromLeader();
+    }
+  }
 
   this.setPlayerName = (id, name) => {
     this.players[id].setName(name);
@@ -19,6 +40,9 @@ function Room() {
   this.handleMessage = msg => {
     this.messages.push(msg);
   };
+
+  // returns an array of players
+  this.getPlayers = () => Object.values(this.players);
 }
 
 module.exports = Room;
