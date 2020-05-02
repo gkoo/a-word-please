cd93eea7a9e9d2a9c3431d162bf8d1d5c61adfea
Gordon Koo <gordon@opendoor.com>
2020-05-02 16:30:19 -0700

    convert Card to prototypal inheritance

--- card.js
+++ card.js
@@ -3,14 +3,16 @@ const { cards, cardLabels, cardNumbers } = require('./constants');
 function Card({ id, type }) {
   this.id = id;
   this.type = type;
-
-  this.getLabel = () => {
-    return cardLabels[this.type];
-  };
-
-  this.getNumber = () => {
-    return cardNumbers[this.type];
-  };
 }
+
+Card.prototype = {
+  getLabel: function() {
+    return cardLabels[this.type];
+  },
+
+  getNumber: function() {
+    return cardNumbers[this.type];
+  },
+};
 
 module.exports = Card;
