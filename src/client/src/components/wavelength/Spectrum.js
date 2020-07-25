48195bfd3194a28531ca23b89177ea2753b7c8ac
Gordon Koo <gordon@opendoor.com>
2020-07-24 19:29:08 -0700

    Revert "Revert "fix wavelength CSS issues""
    This reverts commit bf89208fab2570955fda9393ea62e9c4e2e45a94.

--- src/client/src/components/wavelength/Spectrum.js
+++ src/client/src/components/wavelength/Spectrum.js
@@ -7,47 +7,53 @@ import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 
 import * as selectors from '../../store/selectors';
+import { SPECTRUM_MAX_VALUE, SPECTRUM_BAND_WIDTH } from '../../constants';
 
 function Spectrum({ guessValue, showBands, showSlider, value, bandSelections }) {
   const currConcept = useSelector(selectors.currConceptSelector);
   const spectrumValue = useSelector(selectors.spectrumValueSelector);
-  const leftMargin = (spectrumValue - 25)*100/180;
+
+  const band1LeftBound = (spectrumValue - (SPECTRUM_BAND_WIDTH*5/2));
+  // + 1/4 -----> - 1/4
+  //
+  // Adjust for UI toggle weirdness
+  // very left edge needs to add SPECTRUM_BAND_WIDTH * 1/4
+  // very right edge needs to subtract SPECTRUM_BAND_WIDTH * 1/4
+  const uiFix = SPECTRUM_BAND_WIDTH * (SPECTRUM_MAX_VALUE/2 - spectrumValue)/400;
+  //const band1Midpoint = band1LeftBound + SPECTRUM_BAND_WIDTH/2;
+  const leftMarginPct = (band1LeftBound + uiFix)*100/SPECTRUM_MAX_VALUE;
+  let sliderValueToDisplay;
+
+  if (showSlider) {
+    sliderValueToDisplay = spectrumValue;
+  } else if (guessValue !== undefined && guessValue !== null) {
+    sliderValueToDisplay = guessValue;
+  }
 
   return (
     <>
+      <div className='band-container'>
+        {
+          showBands &&
+            <div className='band-group' style={{ left: `${leftMarginPct}%` }}>
+              <div
+                className={cx('spectrum-band band2', { selected: bandSelections?.firstBand })}
+              />
+              <div className={cx('spectrum-band band3', { selected: bandSelections?.secondBand })}/>
+              <div className={cx('spectrum-band band4', { selected: bandSelections?.thirdBand })}/>
+              <div className={cx('spectrum-band band3', { selected: bandSelections?.fourthBand })}/>
+              <div className={cx('spectrum-band band2', { selected: bandSelections?.fifthBand })}/>
+            </div>
+        }
+      </div>
       {
-        showBands &&
-          <div className='band-group'>
-            <div
-              className={cx('spectrum-band band2', { selected: bandSelections?.firstBand })}
-              style={{ marginLeft: `${leftMargin}%` }}
-            />
-            <div className={cx('spectrum-band band3', { selected: bandSelections?.secondBand })}/>
-            <div className={cx('spectrum-band band4', { selected: bandSelections?.thirdBand })}/>
-            <div className={cx('spectrum-band band3', { selected: bandSelections?.fourthBand })}/>
-            <div className={cx('spectrum-band band2', { selected: bandSelections?.fifthBand })}/>
-          </div>
-      }
-      {
-        showSlider &&
+        (showSlider || (guessValue !== undefined && guessValue !== null)) &&
           <Form>
             <Form.Control
               type="range"
               min={0}
-              max={180}
-              value={spectrumValue}
-              disabled
-            />
-          </Form>
-      }
-      {
-        guessValue &&
-          <Form>
-            <Form.Control
-              type="range"
-              min={0}
-              max={180}
-              value={guessValue}
+              max={SPECTRUM_MAX_VALUE}
+              value={sliderValueToDisplay}
               disabled
             />
           </Form>
