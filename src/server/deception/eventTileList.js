793091389f19444c4459034d47d96cc268d7db76
Gordon Koo <gordon@opendoor.com>
2020-09-09 11:10:32 -0700

    event tiles

---
+++ src/server/deception/eventTileList.js
@@ -0,0 +1,40 @@
+export default [
+  {
+    id: 0,
+    title: 'Secret Testimony',
+    description: 'The witness chooses a scene tile to eliminate. The Forensic Scientist discards ' +
+      'the indicated tile and replaces it with a random one.'
+  },
+  {
+    id: 1,
+    title: 'A Good Twist',
+    description: 'The player who correctly solved the crime last game immediately receives a ' +
+      'chance to do so. This does not count toward their 1 chance this game. If no player is ' +
+      'entitled to this advantage, the Forensic Scientist discards this tile and draws another.'
+  },
+  {
+    id: 2,
+    title: 'A Useful Clue',
+    description: 'The Forensic Scientist draws 5 Scene tiles, from which they choose 1 to ' +
+      'replace any existing tile in the display. The selected tile cannot be another event.'
+  },
+  {
+    id: 3,
+    title: 'Ruled Out',
+    description: 'Each player must flip over 1 of their own Clue Cards, removing it from ' +
+      'consideration. This must be done without discussion. The murderer may not flip over the ' +
+      'Key Evidence.'
+  },
+  {
+    id: 4,
+    title: 'Erroneous Information',
+    description: 'The Forensic Scientist chooses 1 of the scene tiles on display, and moves its ' +
+      'marker to a new item on the same tile.'
+  },
+  {
+    id: 5,
+    title: 'Countdown',
+    description: 'The Forensic Scientist draws two scene tiles and substitutes them for any 2 ' +
+      'scene tiles in the display. The game ends after the following \'Presentation\' is finished.',
+  },
+];
