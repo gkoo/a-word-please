1f93bd084eccb028434d8a5c9e9d232f8476b70b
Gordon Koo <gordon@opendoor.com>
2020-09-09 17:09:40 -0700

    show roles for witness/accomplice/murderer

--- src/client/src/components/deception/PlayerView.js
+++ src/client/src/components/deception/PlayerView.js
@@ -9,7 +9,10 @@ import ClueBadge from './ClueBadge';
 
 import {
   DECEPTION_ROLE_LABELS,
+  ROLE_ACCOMPLICE,
+  ROLE_MURDERER,
   ROLE_SCIENTIST,
+  ROLE_WITNESS,
   STATE_DECEPTION_REPLACE_SCENE,
 } from '../../constants';
 import {
@@ -18,7 +21,11 @@ import {
   socketSelector,
 } from '../../store/selectors';
 
-function PlayerView({ player, showAccuseButtons, showRoles }) {
+function PlayerView({
+  player,
+  showAccuseButtons,
+  showRoles,
+}) {
   const gameData = useSelector(gameDataSelector);
   const currPlayer = useSelector(currPlayerSelector);
   const socket = useSelector(socketSelector);
@@ -40,13 +47,27 @@ function PlayerView({ player, showAccuseButtons, showRoles }) {
     !playerIsCurrPlayer
   );
 
+  const showAccomplice = [
+    ROLE_SCIENTIST,
+    ROLE_ACCOMPLICE,
+    ROLE_MURDERER,
+    ROLE_WITNESS,
+  ].includes(currPlayer?.role);
+  const showMurderer = showAccomplice;
+  const showWitness = [ROLE_SCIENTIST, ROLE_WITNESS].includes(currPlayer?.role);
+
+  let showRole = showRoles;
+  showRole = showRole || (player.role === ROLE_ACCOMPLICE && showAccomplice);
+  showRole = showRole || (player.role === ROLE_MURDERER && showMurderer);
+  showRole = showRole || (player.role === ROLE_WITNESS && showWitness);
+
   return (
     <Card className='deception-player-card my-1'>
       <Card.Body>
         <Card.Title>
           {player.name}
           {
-            showRoles &&
+            showRole &&
               <>
                 <br />
                 <Badge variant='warning'>{DECEPTION_ROLE_LABELS[player.role]}</Badge>
@@ -56,7 +77,7 @@ function PlayerView({ player, showAccuseButtons, showRoles }) {
         <strong>Murder Methods</strong>:{' '}
         {
           player.methodCards.map(card =>
-            <h5>
+            <h5 key={card.label}>
               <ClueBadge type='method' label={card.label} />
             </h5>
           )
@@ -64,7 +85,7 @@ function PlayerView({ player, showAccuseButtons, showRoles }) {
         <strong>Key Evidence</strong>:{' '}
         {
           player.evidenceCards.map(card =>
-            <h5>
+            <h5 key={card.label}>
               <ClueBadge type='evidence' label={card.label} />
             </h5>
           )
