aeb8c139d2b92bc87947f29bcf55da727b4b773a
Gordon Koo <gordon@opendoor.com>
2020-10-31 10:50:58 -0700

    [wavelength] show playersReady when guesses are entered

--- src/client/src/components/common/PlayerCheckboxLabel.js
+++ src/client/src/components/common/PlayerCheckboxLabel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function PlayerCheckboxLabel({ className, checked, player }) {
+function PlayerCheckboxLabel({ checked, player }) {
   return (
     <div className={`checkbox-label player-label text-center mx-1 ${player.color}`}>
       {checked && <span className='checkbox-label-check' role='img' aria-label='checked'>✅</span>}
--- src/client/src/components/wavelength/GuessPhaseGuesserView.js
+++ src/client/src/components/wavelength/GuessPhaseGuesserView.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react'; import { useSelector } from 'react-redux';
 
 import Button from 'react-bootstrap/Button';
 
@@ -7,19 +6,29 @@ import Spectrum from './Spectrum';
 import * as selectors from '../../store/selectors';
 
 function GuessPhaseGuesserView() {
+  const currPlayer = useSelector(selectors.currPlayerSelector);
   const socket = useSelector(selectors.socketSelector);
+  const { playersReady } = useSelector(selectors.gameDataSelector);
 
   const onSubmitGuess = e => {
     e.preventDefault();
     socket.emit('playerAction', { action: 'submitGuess' });
   };
 
+  const submitted = playersReady[currPlayer.id];
+
   return (
     <>
       <Spectrum/>
       <p className='mt-5'>Adjust the slider to where you think this clue falls on the spectrum.</p>
       <div className='text-center'>
-        <Button type='submit' onClick={onSubmitGuess}>Enter guess</Button>
+        <Button
+          type='submit'
+          onClick={onSubmitGuess}
+          disabled={submitted}
+        >
+          { submitted ? 'Waiting for other players...' : 'Enter guess' }
+        </Button>
       </div>
     </>
   );
